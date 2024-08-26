74c33e45b673a47cd0da0822f952b36216147b7f
Rushs321 <fyggh566@tutamail.com>
2024-08-26 10:19:38 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,29 +1,34 @@
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
-function compress(req, res, input) {
+function compress(req, res, inputStream) {
   const format = req.params.webp ? 'webp' : 'jpeg';
 
-  input.pipe(
-    sharp()
-      .grayscale(req.params.grayscale)
-      .toFormat(format, {
-        quality: req.params.quality,
-        progressive: true,
-        optimizeScans: true
-      })
-      .toBuffer((err, output, info) => {
-        if (err || !info || res.headersSent) return redirect(req, res)
+  // Use sharp to process the stream directly
+  const transformStream = sharp()
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true,
+    });
 
-      res.setHeader('content-type', `image/${format}`)
-        res.setHeader('content-length', info.size)
-        res.setHeader('x-original-size', req.params.originSize)
-        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-        res.status(200)
-        res.write(output);
-        res.end();
-      })
-    );
+  // Pipe the input stream into the transformStream (sharp)
+  inputStream
+    .pipe(transformStream)
+    .on('info', (info) => {
+      // Set headers based on the transformed image info
+      res.setHeader('content-type', `image/${format}`);
+      res.setHeader('content-length', info.size);
+      res.setHeader('x-original-size', req.params.originSize);
+      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+      res.status(200);
+    })
+    .on('error', (err) => {
+      // Handle errors and redirect if necessary
+      redirect(req, res);
+    })
+    .pipe(res); // Pipe the transformed image to the response
 }
 
 module.exports = compress;
