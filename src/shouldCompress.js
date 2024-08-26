a1b18a9feb8babf67da91a0710a7b68076418dcb
Rushs321 <fyggh566@tutamail.com>
2024-08-26 10:38:16 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,35 +1,23 @@
-const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
+"use strict";
+const MIN_COMPRESS_LENGTH = 1024
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100
 
 function shouldCompress(req) {
-  const { originType = '', originSize = '0', webp } = req.params;
+  const { originType, originSize, webp } = req.params
 
-  const originTypeLower = originType.toLowerCase();
-  const originSizeNum = parseInt(originSize, 10);
-
-  if (!originTypeLower.startsWith('image')) {
-    console.log('Not an image:', originTypeLower);
-    return false;
-  }
-  if (isNaN(originSizeNum) || originSizeNum === 0) {
-    console.log('Zero size or invalid size:', originSizeNum);
-    return false;
-  }
-  if (webp && originSizeNum < MIN_COMPRESS_LENGTH) {
-    console.log('WebP image too small to compress:', originSizeNum);
-    return false;
-  }
+  if (!originType.startsWith('image')) return false;
+  if (originSize === 0) return false;
+  if (req.headers.range) return false;
+  if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
   if (
     !webp &&
-    (originTypeLower.endsWith('png') || originTypeLower.endsWith('gif')) &&
-    originSizeNum < MIN_TRANSPARENT_COMPRESS_LENGTH
+    (originType.endsWith('png') || originType.endsWith('gif')) &&
+    originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
   ) {
-    console.log('Transparent image too small to compress:', originSizeNum);
     return false;
   }
 
-  console.log('Compression allowed for:', originTypeLower, originSizeNum);
   return true;
 }
 
-module.exports = shouldCompress;
+module.exports = shouldCompress
