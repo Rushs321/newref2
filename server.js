0aeae78061e919503d2aaf1ed57962e68d7b13a9
Rushs321 <fyggh566@tutamail.com>
2024-08-27 17:59:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 #!/usr/bin/env node
 'use strict';
-const fastify = require('fastify')({ logger: true,
-  disableRequestLogging: true,
-  trustProxy: true });
+const fastify = require('fastify')({ logger: true });
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || '0.0.0.0'; // Defaults to '0.0.0.0' for external accessibility
 
-// Fastify hook to handle params as middleware
-fastify.addHook('preHandler', params);
+fastify.register(require('@fastify/express')); // Required for compatibility with some middlewares
 
-// Define the main route
+fastify.addHook('onRequest', params);
+
 fastify.get('/', proxy);
 
-// Handle favicon requests with a 204 No Content response
 fastify.get('/favicon.ico', (req, reply) => reply.status(204).send());
-
 // Start the server
 fastify.listen({ port: PORT, host: HOST }, (err, address) => {
   if (err) {
