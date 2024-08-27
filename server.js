b8027dde6da6c906de05bde827a4feb5e370868a
Rushs321 <fyggh566@tutamail.com>
2024-08-27 17:53:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const params = require('./src/params');
 const proxy = require('./src/proxy');
 
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0'; // Defaults to '0.0.0.0' for external accessibility
 
 // Fastify hook to handle params as middleware
 fastify.addHook('preHandler', params);
@@ -16,7 +17,7 @@ fastify.get('/', proxy);
 fastify.get('/favicon.ico', (req, reply) => reply.status(204).send());
 
 // Start the server
-fastify.listen({ port: PORT }, (err, address) => {
+fastify.listen({ port: PORT, host: HOST }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
