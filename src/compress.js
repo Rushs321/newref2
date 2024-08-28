ad1fb950de9d8fb9154c9e48d3e42c4329099b46
Rushs321 <fyggh566@tutamail.com>
2024-08-28 13:13:31 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,37 +1,30 @@
 const sharp = require('sharp');
 const redirect = require('./redirect');
 
-async function compress(req, reply, input) {
+function compress(req, res, inputStream) {
   const format = req.params.webp ? 'webp' : 'jpeg';
 
-  try {
-    const { data: output, info } = await sharp(input)
-      .grayscale(req.params.grayscale)
-      .toFormat(format, {
-        quality: req.params.quality,
-        progressive: true,
-        optimizeScans: true
-      })
-      .toBuffer({ resolveWithObject: true });
+  const transform = sharp()
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true,
+    });
 
-    if (!info || reply.sent) {
-      return redirect(req, reply);
-    }
+  res.header('content-type', `image/${format}`);
 
-    reply
-      .header('content-type', `image/${format}`)
-      .header('content-length', info.size)
-      .header('x-original-size', req.params.originSize)
-      .header('x-bytes-saved', req.params.originSize - info.size)
-      .status(200)
-      .send(output);
-
-  } catch (err) {
-    // Handle errors and redirect if necessary
-    if (!reply.sent) {
-      redirect(req, reply);
-    }
-  }
+  inputStream
+    .pipe(transform)
+    .on('info', (info) => {
+      res.header('content-length', info.size);
+      res.header('x-original-size', req.params.originSize);
+      res.header('x-bytes-saved', req.params.originSize - info.size);
+    })
+    .on('error', () => {
+      redirect(req, res);
+    })
+    .pipe(res);
 }
 
 module.exports = compress;
